9be01118d7645361d24ad30692778784ed759071
zmainsk <skkim@j2stechno.com>
2024-01-16 17:03:08 +0900

    udemy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import reactImg from "./assets/react-core-concepts.png";
 import componentsImg from "./assets/components.png";
 
+import data from "./data.js";
+
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
@@ -40,21 +42,7 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept
-              title="Components"
-              description="The core UI building block."
-              image={componentsImg}
-            />
-            <CoreConcept
-              title="Components"
-              description="The core UI building block."
-              image={componentsImg}
-            />
-            <CoreConcept
-              title="Components"
-              description="The core UI building block."
-              image={componentsImg}
-            />
+            {console.log(data)}
             <CoreConcept
               title="Components"
               description="The core UI building block."
